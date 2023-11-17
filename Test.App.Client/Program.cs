58385c4bbcdeaef8167ba84b6a156061d059b46e
Brad Browne <brad@mapdojo.com>
2023-11-18 00:36:45 +1100

    Added additional comment

--- Test.App.Client/Program.cs
+++ Test.App.Client/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
+// FIXME: This is also required (which I expect) but why is it also required in Test.App?! 
 builder.Services.AddSingleton<TestViewModel>();
 
 await builder.Build().RunAsync();
